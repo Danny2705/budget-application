15ec2e8901a6ade6e2245303ae5f101421f5c351
Jesse Thomas <jesseayo25@gmail.com>
2024-05-14 13:23:57 -0600

    publish branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
 import Dashboard from "./screens/Dashboard/Dashboard";
 import Transaction from "./screens/Transaction/Transaction";
-import Budget from "./screens/Budget/Budget";
+import Budget from "./screens/Budget/budget";
 import Profile from "./screens/Profile/Profile";
 
 function App() {
