a3541961d1936d1f0279a785615299ee6a9701b5
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-05-19 12:04:52 -0600

    update table stlying, add fontawsome package, add incons

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -1,3 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons";
+
+const tableHead = [
+  { name: "Transaction No." },
+  { name: "Vender" },
+  { name: "Date" },
+  { name: "Category" },
+  { name: "Total(CAD$)" },
+  { name: "Action" },
+];
+
 const transactionData = [
   {
     TransactionNo: "2465343",
@@ -43,48 +55,71 @@ const transactionData = [
   },
 ];
 
-const tableHead = [
-  { name: "Transaction No." },
-  { name: "Vender" },
-  { name: "Date" },
-  { name: "Category" },
-  { name: "Total(CAD$)" },
-  { name: "Action" },
-];
-
 export default function TransactionTable() {
   return (
-    <table classname="bg-[#26264F], text-white">
-      <thead>
-        <tr>
-          {tableHead.map((val, key) => {
+    <div className="py-[48px] flex justify-center">
+      <table className="bg-[#26264F] text-white border-separate border rounded-lg">
+        <thead>
+          <tr>
+            {/* control boarder radius of the table head
+            reference: https://stackoverflow.com/questions/4932181/rounded-table-corners-css-only 
+            ps. from 18Answer
+            */}
+            <th className="border-b px-[64px] py-2 bg-[#1D1E42] text-white">
+              Transaction No.
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Vender
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Date
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Category
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Total(CAD$)
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-white">
+          {transactionData.map((val, key) => {
             return (
-              <th
+              <tr
                 key={key}
-                className="border px-4 py-2 bg-[#26264F] text-white"
+                className={key % 2 === 0 ? "bg-[#26264F]" : "bg-[#1D1E42]"}
               >
-                {val.name}
-              </th>
+                <td className="border-b px-4 py-2 text-center">
+                  {val.TransactionNo}
+                </td>
+                <td className="border-b border-l px-4 py-2 text-center">
+                  {val.Vender}
+                </td>
+                <td className="border-b border-l px-4 py-2 text-center">
+                  {val.Date}
+                </td>
+                <td className="border-b border-l px-4 py-2 text-center">
+                  {val.Category}
+                </td>
+                <td className="border-b border-l px-[32px] py-2">{val.Total}</td>
+                <td className="border-b border-l px-4 py-2">
+                  <div className="flex flex-row justify-center">
+                    <button className="pr-[32px]">
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                    <button>
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
             );
           })}
-        </tr>
-      </thead>
-      <tbody className="text-white">
-        {transactionData.map((val, key) => {
-          return (
-            <tr
-              key={key}
-              className={key % 2 === 0 ? "bg-[#26264F]" : "bg-[#1D1E42]"}
-            >
-              <td className="border px-4 py-2">{val.TransactionNo}</td>
-              <td className="border px-4 py-2">{val.Vender}</td>
-              <td className="border px-4 py-2">{val.Date}</td>
-              <td className="border px-4 py-2">{val.Category}</td>
-              <td className="border px-4 py-2">{val.Total}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
