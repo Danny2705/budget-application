c1a7fcff77ed720dc055ba214f2ca2eb66cccc87
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-05-19 10:19:11 -0600

    created Transaction Table w/o styling and w/o action icons

---
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -0,0 +1,40 @@
+
+const transactionData = [
+    {TransactionNo: "2465343", Vender: "Walgreens", Date: "03/28/2023", Category: "Grocery", Total: "$50.00"},
+    {TransactionNo: "2465344", Vender: "Walmart", Date: "03/29/2023", Category: "Food", Total: "$60.00"},
+    {TransactionNo: "2465345", Vender: "AB Clean", Date: "03/30/2023", Category: "Housing", Total: "$70.00"},
+    {TransactionNo: "2465346", Vender: "Walgreens", Date: "03/31/2023", Category: "Grocery", Total: "$50.00"},
+    {TransactionNo: "2465347", Vender: "Walmart", Date: "04/01/2023", Category: "Food", Total: "$60.00"},
+    {TransactionNo: "2465348", Vender: "CC Car Wash", Date: "04/02/2023", Category: "Car", Total: "$30.00"},
+];
+
+export default function TransactionTable() {
+    return (
+        <table>
+        <thead>
+            <tr>
+                <th className="px-4 py-2 bg-green-500 text-white">Transaction No.</th>
+                <th className="px-4 py-2 bg-green-500 text-white">Vender</th>
+                <th className="px-4 py-2 bg-green-500 text-white">Date</th>
+                <th className="px-4 py-2 bg-green-500 text-white">Category</th>
+                <th className="px-4 py-2 bg-green-500 text-white">Total(CAD$)</th>
+                <th className="px-4 py-2 bg-green-500 text-white">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            {transactionData.map((val, key) => {
+                return (
+                    <tr key={key} className={key % 2 === 0 ? 'bg-gray-200' : 'bg-white'}>
+                        <td className="border px-4 py-2">{val.TransactionNo}</td>
+                        <td className="border px-4 py-2">{val.Vender}</td>
+                        <td className="border px-4 py-2">{val.Date}</td>
+                        <td className="border px-4 py-2">{val.Category}</td>
+                        <td className="border px-4 py-2">{val.Total}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+    </table>
+
+    );
+  }
