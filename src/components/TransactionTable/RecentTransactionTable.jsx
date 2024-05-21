54360bbb1693f489fe2f879f8dd0d24f677ee171
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-05-20 21:32:30 -0700

    icons added color

--- src/components/TransactionTable/RecentTransactionTable.jsx
+++ src/components/TransactionTable/RecentTransactionTable.jsx
@@ -55,16 +55,14 @@ const transactionData = [
   },
 ];
 
-export default function TransactionTable() {
+export default function RecentTransactionTable() {
   return (
     <div className="py-[48px] flex justify-center">
       <table className="bg-[#26264F] text-white border-separate border rounded-lg">
         <thead>
           <tr>
-            {/* control boarder radius of the table head
-            reference: https://stackoverflow.com/questions/4932181/rounded-table-corners-css-only 
-            ps. from 18Answer
-            */}
+            {/* control boarder radius of the table head; refer from 
+            https://stackoverflow.com/questions/4932181/rounded-table-corners-css-only */}
             <th className="border-b px-[64px] py-2 bg-[#1D1E42] text-white">
               Transaction No.
             </th>
@@ -88,6 +86,9 @@ export default function TransactionTable() {
         <tbody className="text-white">
           {transactionData.map((val, key) => {
             return (
+              /* switch color of the row refer from https://chatgpt.com/
+            bg-[#26264F] and bg-[#1D1E42] are the colors used in the Figma Design
+            */
               <tr
                 key={key}
                 className={key % 2 === 0 ? "bg-[#26264F]" : "bg-[#1D1E42]"}
@@ -104,14 +105,24 @@ export default function TransactionTable() {
                 <td className="border-b border-l px-4 py-2 text-center">
                   {val.Category}
                 </td>
-                <td className="border-b border-l px-[32px] py-2">{val.Total}</td>
+                <td className="border-b border-l px-[32px] py-2">
+                  {val.Total}
+                </td>
                 <td className="border-b border-l px-4 py-2">
                   <div className="flex flex-row justify-center">
                     <button className="pr-[32px]">
-                      <FontAwesomeIcon icon={faPenToSquare} />
+                      {/* #624DE3 is the color in the Figna Design */}
+                      <FontAwesomeIcon
+                        icon={faPenToSquare}
+                        style={{ color: "#624DE3" }}
+                      />
                     </button>
                     <button>
-                      <FontAwesomeIcon icon={faTrashCan} />
+                      {/* #A30D11 is the color in the Figna Design */}
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        style={{ color: "#A30D11" }}
+                      />
                     </button>
                   </div>
                 </td>
