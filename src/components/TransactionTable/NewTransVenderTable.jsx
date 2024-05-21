e37372ba78a765e0eb2bb4f283f2e324b51bcd0c
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-05-20 22:18:18 -0700

    created NewTransVenderTable

--- src/components/TransactionTable/NewTransVenderTable.jsx
+++ src/components/TransactionTable/NewTransVenderTable.jsx
@@ -1,61 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
-const tableHead = [
-  { name: "Transaction No." },
-  { name: "Vender" },
-  { name: "Date" },
-  { name: "Category" },
-  { name: "Total(CAD$)" },
-  { name: "Action" },
-];
-
 const transactionData = [
   {
-    TransactionNo: "2465343",
-    Vender: "Walgreens",
+    Vender: "Walmart",
     Date: "03/28/2023",
-    Category: "Grocery",
-    Total: "$50.00",
-  },
-  {
-    TransactionNo: "2465344",
-    Vender: "Walmart",
-    Date: "03/29/2023",
-    Category: "Food",
-    Total: "$60.00",
-  },
-  {
-    TransactionNo: "2465345",
-    Vender: "AB Clean",
-    Date: "03/30/2023",
-    Category: "Housing",
-    Total: "$70.00",
-  },
-  {
-    TransactionNo: "2465346",
-    Vender: "Walgreens",
-    Date: "03/31/2023",
-    Category: "Grocery",
-    Total: "$50.00",
-  },
-  {
-    TransactionNo: "2465347",
-    Vender: "Walmart",
-    Date: "04/01/2023",
-    Category: "Food",
-    Total: "$60.00",
-  },
-  {
-    TransactionNo: "2465348",
-    Vender: "CC Car Wash",
-    Date: "04/02/2023",
-    Category: "Car",
-    Total: "$30.00",
-  },
+    Location: "Calgary, CA",
+    Category: "Food & Drink",
+    Subtotal: "27.60",
+    Tax: "1.93",
+    Total: "29.53",
+  }
 ];
 
-export default function RecentTransactionTable() {
+export default function NewTransVenderTable() {
   return (
     <div className="py-[48px] flex justify-center">
       <table className="bg-[#26264F] text-white border-separate border rounded-lg">
@@ -64,16 +22,22 @@ export default function RecentTransactionTable() {
             {/* control boarder radius of the table head; refer from 
             https://stackoverflow.com/questions/4932181/rounded-table-corners-css-only */}
             <th className="border-b px-[64px] py-2 bg-[#1D1E42] text-white">
-              Transaction No.
-            </th>
-            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
               Vender
             </th>
             <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
               Date
             </th>
             <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Location
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
               Category
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Subtotal(CAD$)
+            </th>
+            <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
+              Tax(CAD$)
             </th>
             <th className="border-b border-l px-[64px] py-2 bg-[#1D1E42] text-white">
               Total(CAD$)
@@ -94,16 +58,22 @@ export default function RecentTransactionTable() {
                 className={key % 2 === 0 ? "bg-[#26264F]" : "bg-[#1D1E42]"}
               >
                 <td className="border-b px-4 py-2 text-center">
-                  {val.TransactionNo}
-                </td>
-                <td className="border-b border-l px-4 py-2 text-center">
                   {val.Vender}
                 </td>
                 <td className="border-b border-l px-4 py-2 text-center">
                   {val.Date}
                 </td>
                 <td className="border-b border-l px-4 py-2 text-center">
+                  {val.Location}
+                </td>
+                <td className="border-b border-l px-4 py-2 text-center">
                   {val.Category}
+                </td>
+                <td className="border-b border-l px-[32px] py-2">
+                  {val.Subtotal}
+                </td>
+                <td className="border-b border-l px-[32px] py-2">
+                  {val.Tax}
                 </td>
                 <td className="border-b border-l px-[32px] py-2">
                   {val.Total}
