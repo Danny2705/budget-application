cc0a3f7d629e2d4d82cbbaef106d7692a0bc05e0
Jesse Thomas <jesseayo25@gmail.com>
2024-05-22 07:55:57 -0600

    changed files from budget to transaction

--- src/screens/Budget/Budget.jsx
+++ src/screens/Budget/Budget.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Layout from "../Layout/Layout";
 
-// TODO: see calendar component render on page
-// REMOVE component and insert where needed
-import NewBudget from "../../components/NewBudget";
-
 export default function Budget() {
   return (
     <Layout>
       <h1 className='mt-[60px] text-white'>Budget</h1>
       <div>
-        <NewBudget />
       </div>
     </Layout>
   );
