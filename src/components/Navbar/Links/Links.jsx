af823b440eb5104b7212605bc6a6b1c67631cf4d
Danny <nguyencaodat270504@gmail.com>
2024-05-28 12:46:41 -0600

    Update budget page responsive in small screen

--- src/components/Navbar/Links/Links.jsx
+++ src/components/Navbar/Links/Links.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
 
-const paths = ["/", "/transaction", "/budget"];
 const Links = () => {
   const location = useLocation();
   const [selected, setSelected] = useState(location.pathname);
@@ -11,44 +9,41 @@ const Links = () => {
     setSelected(path);
   };
 
-  const linkVariants = {
-    initial: { y: 0 },
-    hover: { y: -10 },
-    selected: { y: -10 },
-  };
-
-  const linkTransition = {
-    type: "spring",
-    stiffness: 200,
-    damping: 30,
-  };
-
   return (
     <div className='flex items-center gap-8 h-full px-4 rounded-full'>
-      {paths.map((path) => (
-        <Link
-          key={path}
-          to={path}
-          onClick={() => handleSelect(path)}
-          className={`${
-            selected === path
-              ? "border-b-2 border-main-neonPink shadow-md shadow-main-neonPink text-main-neonPink"
-              : "text-white hover:text-main-neonPink"
-          } text-sm transition-colors px-2.5 py-0.5 font-medium rounded-md relative`}
-        >
-          <motion.div
-            initial='initial'
-            whileHover={selected === path ? "selected" : "hover"}
-            animate={selected === path ? "selected" : "initial"}
-            variants={linkVariants}
-            transition={linkTransition}
-          >
-            {path === "/"
-              ? "Home"
-              : path.substring(1).charAt(0).toUpperCase() + path.substring(2)}
-          </motion.div>
-        </Link>
-      ))}
+      <Link
+        to='/'
+        onClick={() => handleSelect("/")}
+        className={`${
+          selected === "/"
+            ? "border-b-2 border-main-neonPink shadow-top-md text-main-neonPink"
+            : "text-white hover:text-main-neonPink"
+        } text-lg transition-colors px-2.5 py-0.5 font-medium rounded-md relative`}
+      >
+        Home
+      </Link>
+      <Link
+        to='/transaction'
+        onClick={() => handleSelect("/transaction")}
+        className={`${
+          selected === "/transaction"
+            ? "border-b-2 border-main-neonPink shadow-top-md text-main-neonPink"
+            : "text-white hover:text-main-neonPink"
+        } text-lg transition-colors px-2.5 py-0.5 font-medium rounded-md relative`}
+      >
+        Transaction
+      </Link>
+      <Link
+        to='/budget'
+        onClick={() => handleSelect("/budget")}
+        className={`${
+          selected === "/budget"
+            ? "border-b-2 border-main-neonPink shadow-top-md text-main-neonPink"
+            : "text-white hover:text-main-neonPink"
+        } text-lg transition-colors px-2.5 py-0.5 font-medium rounded-md relative`}
+      >
+        Budget Management
+      </Link>
     </div>
   );
 };
