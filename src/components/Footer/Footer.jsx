667c411e1dd1f5c2a205c182ee38e37f2d2f6112
Danny <nguyencaodat270504@gmail.com>
2024-06-05 12:28:53 -0600

    Change footer padding

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <section className='py-10 bg-black sm:pt-16 lg:pt-24'>
-      <div className='mx-auto sm:px-6 max-w-7xl'>
+      <div className='mx-auto sm:px-6 w-full px-4 xl:px-20'>
         <div className='grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12'>
           <div className='col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8'>
             <div className='flex items-center gap-3'>
