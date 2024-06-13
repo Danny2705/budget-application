a9557a4390c288b83540b8410166435d6cf01883
Jesse Thomas <jesseayo25@gmail.com>
2024-06-13 13:02:06 -0600

    chencged File name

--- src/screens/Profile/Avatar.jsx
+++ src/screens/Profile/Avatar.jsx
@@ -3,7 +3,7 @@ import Avatar from "react-avatar";
 export default function ProfileAvatar({ image, name, size }) {
     return (
       <div>
-        <Avatar name={name} src={image} size={!size ? ("100") : ("50")} round={true} color="#801AE5" />
+        <Avatar name={name} src={image} size={!size ? ("100") : ("40")} round={true} color="#801AE5" />
       </div>
     );
   }
