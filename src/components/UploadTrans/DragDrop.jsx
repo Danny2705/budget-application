c4a1af561c8798854e3d5587c9bbb3b0585143ae
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-07-02 16:40:59 -0600

    DragDrop accepts maximum 10 images, displays file names, added merging images sunction-mergeImage

--- src/components/UploadTrans/DragDrop.jsx
+++ src/components/UploadTrans/DragDrop.jsx
@@ -15,6 +15,39 @@ const DragDrop = ({ onSetImageURL, onSetJsonData, onSetTransactionNo }) => {
   const [transactionNo, setTransactionNo] = useState("");
   const [uploadedFile, setUploadedFile] = useState([]);
 
+  //merging the images
+  const mergeImages = (files) => {
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const images = [];
+    let maxImageWidth = 0;
+    let totalImageHeight = 0;
+
+    return new Promise((resolve, reject) => {
+      files.forEach((file) => {
+        const img = new Image();
+        img.src = URL.createObjectURL(file);
+        img.onload = () => {
+          images.push(img);
+          maxImageWidth = Math.max(maxImageWidth, img.width);
+          totalImageHeight += img.height;
+
+          if (images.length === files.length) {
+            canvas.width = maxImageWidth;
+            canvas.height = totalImageHeight;
+
+            let y = 0;
+            images.forEach((image) => {
+              ctx.drawImage(image, 0, y);
+              y += image.height;
+            });
+
+            resolve(canvas.toDataURL("image/jpeg"));
+          }
+        };
+      });
+    });
+  };
   // Refers from Demo
   const storeAndConvertReceiptImage = async (droppedfile) => {
     // uploadImageToFirestore(localImage) return { transactionNumber, imageURL }
@@ -29,30 +62,36 @@ const DragDrop = ({ onSetImageURL, onSetJsonData, onSetTransactionNo }) => {
     console.log("Transaction no from Firebase", transactionNumber);
 
     // Calling OCR Perform Function
-      if (imageURL) {
-        setReceiptJsonData(await performOcr(imageURL));
-        onSetJsonData(await performOcr(imageURL));
-      }
+    if (imageURL) {
+      setReceiptJsonData(await performOcr(imageURL));
+      onSetJsonData(await performOcr(imageURL));
+    }
   };
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "image/jpeg": [],
+      "image/jpg": [],
       "image/png": [],
+      "file/pdf": [],
     },
-    maxFiles: 1,
+    maxFiles: 10,
     onDrop: (acceptedFiles) => {
-      setUploadedFile(acceptedFiles[0]);
-      storeAndConvertReceiptImage(acceptedFiles[0]);
+      setUploadedFile(acceptedFiles);
+      storeAndConvertReceiptImage(mergeImages(acceptedFiles));
     },
   });
 
   return (
     <div className="bg-main-neonPink my-[16px] mx-[32px] rounded-[16px] h-[80px] flex justify-center items-center text-white font-semibold hover:bg-neon-pink">
-      <div {...getRootProps()} >
+      <div {...getRootProps()}>
         <input {...getInputProps()} />
         <p>Drag and drop files here or click to browse.</p>
-        <span >{uploadedFile.name}</span>
+        {uploadedFile.map((file) => (
+          <span key={file.path}>
+            {file.path} {(file.size / 1048576).toFixed(2)} MB <br />
+          </span>
+        ))}
       </div>
     </div>
   );
