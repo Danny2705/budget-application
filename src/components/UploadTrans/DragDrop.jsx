61e0e05c6abdac44be40f6721a8d55c7b1afaa78
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-07-03 19:07:31 -0600

    merge images by height

--- src/components/UploadTrans/DragDrop.jsx
+++ src/components/UploadTrans/DragDrop.jsx
@@ -21,30 +21,65 @@ const DragDrop = ({ onSetImageURL, onSetJsonData, onSetTransactionNo }) => {
   //merging the images
   //https://www.npmjs.com/package/merge-images
   const runMergeImages = async (files) => {
-    const urls = files.map(URL.createObjectURL)
+    const urls = files.map(URL.createObjectURL);
+    console.log("runMergeImages files", files);
+    console.log("runMergeImages urls", urls);
     try {
-      setImageB64(await mergeImages(urls));
+      //https://stackoverflow.com/questions/623172/how-to-get-the-image-size-height-width-using-javascript
+      const imageHeights = await Promise.all(
+        urls.map((url) => {
+          return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => {
+              resolve(img.height);
+            };
+            img.onerror = () => {
+              reject("Failed to load image");
+            };
+            img.src = url;
+          });
+        })
+      );
+      console.log("Image heights:", imageHeights);
+
+      //merge images based on height
+      const imagesForMerge = urls.map((url, index) => ({
+        src: url,
+        y: imageHeights.slice(0, index).reduce((a, b) => a + b, 0),
+      }));
+      const mergedImage = await mergeImages(imagesForMerge, {
+        height: imageHeights.reduce((a, b) => a + b, 0),
+      });
+
+      setImageB64(mergedImage);
     } catch (error) {
-      console.error('error merging images', error)
+      console.error("error merging images", error);
     } finally {
       //https://stackoverflow.com/questions/6765370/merge-image-using-javascript
-      urls.forEach(URL.revokeObjectURL)
+      urls.forEach(URL.revokeObjectURL);
     }
   };
   //https://stackoverflow.com/questions/43358456/convert-image-uri-into-javascript-file-object
   const dataURLtoFile = (dataUrl, fileName) => {
-    let arr = dataUrl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    let arr = dataUrl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
     while (n--) {
       u8arr[n] = bstr.charCodeAt(n);
     }
     return new File([u8arr], fileName, { type: mime });
   };
-
+  useEffect(() => {
+    if (uploadedFiles.length > 0) {
+      runMergeImages(uploadedFiles);
+    }
+  }, [uploadedFiles]);
 
   useEffect(() => {
     if (imageB64 !== "") {
-      const file = dataURLtoFile(imageB64, 'mergedReceipt.png');
+      const file = dataURLtoFile(imageB64, "mergedReceipt.png");
       storeAndConvertReceiptImage(file);
       console.log("imageB64", file);
     }
@@ -63,12 +98,12 @@ const DragDrop = ({ onSetImageURL, onSetJsonData, onSetTransactionNo }) => {
     console.log("Image uploaded to storage", imageURL);
     console.log("Transaction no from Firebase", transactionNumber);
 
-    // Calling OCR Perform Function
-    if (imageURL) {
-      setReceiptJsonData(await performOcr(imageURL));
-      onSetJsonData(await performOcr(imageURL));
-    }
-  };
+    //Calling OCR Perform Function
+  //   if (imageURL) {
+  //     setReceiptJsonData(await performOcr(imageURL));
+  //     onSetJsonData(await performOcr(imageURL));
+  //   }
+  // };
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
@@ -80,16 +115,15 @@ const DragDrop = ({ onSetImageURL, onSetJsonData, onSetTransactionNo }) => {
     maxFiles: 10,
     onDrop: (acceptedFiles) => {
       setUploadedFiles(acceptedFiles);
-      runMergeImages(acceptedFiles);
+      // runMergeImages(acceptedFiles);
       console.log("acceptedFiles", acceptedFiles);
-      // storeAndConvertReceiptImage(acceptedFiles);
     },
   });
 
   return (
-    <div className="bg-main-neonPink my-[16px] mx-[32px] rounded-[16px] h-[80px] flex justify-center items-center text-white font-semibold hover:bg-neon-pink">
+    <div className="bg-main-neonPink  my-[16px] mx-[32px] rounded-[16px] py-[16px] flex justify-center items-center text-white font-semibold hover:bg-neon-pink">
       <div {...getRootProps()}>
-        <input {...getInputProps()} />
+        <input {...getInputProps()} className="my-[16px]" />
         <p>Drag and drop files here or click to browse.</p>
         {uploadedFiles.map((file) => (
           <span key={file.path}>
