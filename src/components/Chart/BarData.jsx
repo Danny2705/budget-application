947f769a8201188e814b8718961942889b31791e
Jesse Thomas <jesseayo25@gmail.com>
2024-07-12 12:52:28 -0600

    modified components

--- src/components/Chart/BarData.jsx
+++ src/components/Chart/BarData.jsx
@@ -1,11 +1,9 @@
-// Reference from ChatGPT [Modified]: {i want to be able to calculate and accumulate the total money the user spend}
-
 import { useState, useEffect } from "react";
-import { transactionData } from "../TransactionTable/Data";
-import { subMonths, format, formatDate } from "date-fns";
-import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { subMonths, format } from "date-fns";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../utils/firebase.js";
 import { useDispatch, useSelector } from "react-redux";
+import { transactionData } from "../TransactionTable/Data.jsx";
 
 export default function useExpenseData() {
   const [labels, setLabels] = useState([]);
@@ -15,50 +13,64 @@ export default function useExpenseData() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const getMonthFromTimeStamp = (seconds) => {
+      if (seconds) {
+        const date = new Date(seconds * 1000);
+      }'0.
+        '
+      formatDate(date, "MMMM yyyy")
+      return format(date, "MMMM yyyy");
+    };
+
     const FetchBudgetData = async () => {
       const docRef = await getDocs(collection(db, `users/${user.uid}/budget`));
       const budgetData = docRef.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      const month =  budgetData.map((data, index) => getMonthFromTimeStamp(data.endDate.seconds));
-console.log(month);
+      const monthAmounts = budgetData.map((data) => ({
+        Date: getMonthFromTimeStamp(data.endDate.seconds),
+        Total: parseFloat(data.amount),
+      }));
+      console.log('Budget Data:', monthAmounts);
+      return monthAmounts;
     };
 
     const getMonths = () => {
       const months = [];
       for (let i = 4; i >= 0; i--) {
-        months.push(format(subMonths(new Date(), i), "MMMM"));
+        months.push(format(subMonths(new Date(), i), "MMMM yyyy"));
       }
       setLabels(months);
     };
 
-    const getMonthFromTimeStamp = (seconds) => {
-        const date = new Date(seconds * 1000);
-        return format(date, 'm yy');
-    }
     const calculateMonthlyMoney = (data) => {
       const monthlyTotals = {};
       data.forEach((transaction) => {
-        const date = new Date(transaction.Date);
-        const monthYear = format(date, "MMMM");
-
-        if (!monthlyTotals[monthYear]) {
-          monthlyTotals[monthYear] = 0;
+        const month = transaction.Date;
+        if (!monthlyTotals[month]) {
+          monthlyTotals[month] = 0;
         }
-
-        monthlyTotals[monthYear] += parseFloat(transaction.Total);
+        monthlyTotals[month] += transaction.Total;
       });
-
-      return monthlyTotals;
+      return (monthlyTotals);
     };
 
-    getMonths();
-    setTotalMoneySpent(calculateMonthlyMoney(transactionData));
-    // setTotalBudgetLimit(calculateMonthlyMoney(FetchBudgetData()));
-    FetchBudgetData();
-    // console.log(totalBudgetLimit);
-    // console.log(totalMoneySpent);
+    const fetchAndCalculate = async () => {
+      getMonths();
+
+      const totalMoneySpentResult = calculateMonthlyMoney(transactionData);
+      setTotalMoneySpent(totalMoneySpentResult);
+
+      const budgetData = await FetchBudgetData();
+      const totalBudgetLimitResult = calculateMonthlyMoney(budgetData);
+      setTotalBudgetLimit(totalBudgetLimitResult);
+
+      console.log('Budget Limit:', totalBudgetLimitResult);
+      console.log('Money Spent:', totalMoneySpentResult);
+    };
+
+    fetchAndCalculate();
   }, [user, dispatch]);
 
   return { labels, totalMoneySpent, totalBudgetLimit };
