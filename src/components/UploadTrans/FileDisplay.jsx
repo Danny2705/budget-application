8dd1914d3843005200a812905af0c8367c7d95f2
Jessie <chonthicha.pai-ngam@edu.sait.ca>
2024-08-05 23:34:35 -0600

    Cleanned up coding

--- src/components/UploadTrans/FileDisplay.jsx
+++ src/components/UploadTrans/FileDisplay.jsx
@@ -1,15 +1,3 @@
-//Refers from Demo
-// const FileDisplay = ({ imageSrc }) => {
-//   return (
-//     <div className="relative w-[560px] h-[560px] bg-gray-300 shadow-2xl rounded-[8px] overflow-y-auto">
-//       <div className="flex justify-center py-[8px]">
-//           {imageSrc && <img src={imageSrc} alt="Uploaded receipt" />}
-//       </div>
-//     </div>
-//   );
-// };
-// export default FileDisplay;
-
 import React, { useEffect, useState } from "react";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
